f1927ee2fcd39e5fcc4962909d5ccc0172a6d035
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-07-02 16:30:55 +0900

    include FactoryGirl::Syntax::Methods to rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include FactoryGirl::Syntax::Methods
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
