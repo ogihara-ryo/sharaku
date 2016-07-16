9a0aceea96f4bd5b3e55ce094edf43453b125b51
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-07-17 02:43:46 +0900

    Set association for topic belongs to category

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -23,4 +23,15 @@ RSpec.describe Topic, type: :model do
     it { is_expected.to respond_to(:created_at) }
     it { is_expected.to respond_to(:updated_at) }
   end
+
+  describe 'Association' do
+    describe 'belongs_to :category' do
+      subject { topic.category }
+
+      let(:category) { create(:topic_category) }
+      let(:topic) { create(:topic, category: category) }
+
+      it { is_expected.to eq category }
+    end
+  end
 end
